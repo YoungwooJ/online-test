a4846a1ced28800f88a82a810087ed2f200cb7cc
YoungwooJ <cyw960714@naver.com>
2023-02-08 14:15:19 +0900

    시험, 문제, 보기, 답안 필터 생성 / index 페이지 생성

---
+++ online-test/src/main/java/goodee/gdj58/online/filter/QuestionFilter.java
@@ -0,0 +1,44 @@
+package goodee.gdj58.online.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j // static log log = new log();
+@WebFilter("/question/*")
+public class QuestionFilter implements Filter{
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		log.debug("QuestionFilter");
+		
+		if(request instanceof HttpServletRequest) {
+			HttpServletRequest req = (HttpServletRequest)request;
+			HttpSession session = ((HttpServletRequest) request).getSession();
+			
+			if(session.getAttribute("loginStudent") == null && session.getAttribute("loginTeacher") == null) {
+				((HttpServletResponse)response).sendRedirect(req.getContextPath()+"/index");
+				return;
+			}
+		} else {
+			log.debug("웹브라우저 요청만 허용합니다.");
+			return;
+		}
+		
+		// controller 전
+		chain.doFilter(request, response);
+		// controller 후
+		
+	}
+}
